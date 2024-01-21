01942ad02dec4ffea3ef2d53a65203b6ab71fbfc
keerthi vasan <keerthivasan110803@gmail.com>
2024-01-21 17:57:59 +0530

    modified addpost for feeds and blogs and moved the chatbot location

--- blogs/models.py
+++ blogs/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 from datetime import datetime
 
 # Create your models here.
 class Blog(models.Model):
     heading = models.CharField(max_length=200)
     textContent = models.TextField()
+    blogSlug = models.SlugField(unique=True)
     imageContent = models.ImageField(upload_to='post', null=True)
     postedBy = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     postedOn = models.DateTimeField(default=datetime.now(), blank=True)
+
+    def save(self, *args, **kwargs):  # new
+        if not self.blogSlug:
+            self.blogSlug = slugify(self.heading)
+        return super().save(*args, **kwargs)
--- blogs/views.py
+++ blogs/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
+from . import models
 
 # Create your views here.
 def blogView(request):
-    return render(request, 'blog.html')
+    context = dict()
+    blogs = models.Blog.objects.all()
+    context['blogs'] = blogs
+    print(blogs)
+    return render(request, 'blog.html', context)
 
 def fullBlogView(request, slug):
-    return render(request, 'fullBlog.html')
+    context = {}
+    blogs = models.Blog.objects.get(blogSlug=slug)
+    context['blog'] = blogs
+    
+    return render(request, 'fullBlog.html', context)
 
 def postFormView(request):
     context = {}
+    user = request.user
     if request.method == "POST":
-        pass
+        form = forms.postform(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            heading = form.cleaned_data.get('heading')
+            textContent = form.cleaned_data.get('text')
+            imageContent = form.cleaned_data.get('image')
+            obj = models.Blog.objects.create(heading=heading,
+                                            textContent = textContent, 
+                                             postedBy = user,
+                                             imageContent = imageContent)
+            obj.save()
+            return redirect('feeds:home')
     context['form'] = forms.postform()
     return render(request, 'postform.html', context)
