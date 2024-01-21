177201c278ecd1d40758871afd347abfaa44b5a8
keerthi vasan <keerthivasan110803@gmail.com>
2024-01-21 10:59:24 +0530

    added events

--- genz/urls.py
+++ genz/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feeds.urls')),
     path('blogs/', include('blogs.urls')),
+    path('events/', include('events.urls')),
 ]
 
 if settings.DEBUG:
